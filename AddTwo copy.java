18b01dc93966ebaac86cfaf2560f9b32d8853870
ronicohen2 <154340689+ronicohen2@users.noreply.github.com>
2023-12-21 07:51:02 +0200

    add homework 1

---
+++ AddTwo copy.java
@@ -0,0 +1,9 @@
+public class AddTwo
+{
+	public static void main(String[] args)
+	{
+		int x = Integer.parseInt(args[0]);
+		int y = Integer.parseInt(args[1]);
+		System.out.println(x + " + " + y + " = " + (x+y));
+	}
+}
---
+++ Triangle copy.java
@@ -0,0 +1,22 @@
+public class Triangle
+{
+	public static void main(String[] args)
+	{
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int c = Integer.parseInt(args[2]);
+		if ((a + b) < c )
+		{
+	       System.out.println(a + " , " + b + " , " + c + ":false");
+		}
+		else if ((a+c) < b)
+		{
+			System.out.println(a + " , " + b + " , " + c + ":false");
+		}
+		else if ((b+c) < a)
+		{
+			System.out.println(a + " , " + b + " , " + c + ":false");
+		}
+		else System.out.println(a + " , " + b + " , " + c + ":true");
+	}
+}
