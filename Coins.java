708cc3ea39ba87011796e7df08b7aef20ada49cc
ronicohen2 <154340689+ronicohen2@users.noreply.github.com>
2023-12-21 15:18:38 +0200

    change HW1

--- Coins.java
+++ Coins.java
@@ -1,9 +1,11 @@
-/*
- * Write a program that gets a quantity of cents as a command-line argument.
- * The program prints how to represent this quantity using as many quarters as possible, plus the remainder in cents.
- */
-public class Coins {
-	public static void main(String[] args) {
-		// Put your code here
+public class Coins
+{
+	public static void main(String[] args)
+	{
+		int x = Integer.parseInt(args[0]);
+		int qua, cen;
+		qua = x/25;
+		cen = x%25;
+		System.out.println("Use " + qua + " quarters and " + cen + " cents ");
 	}
 }
--- GenThree.java
+++ GenThree.java
@@ -1,9 +1,17 @@
-/*
- * Generates three random integers, each in a given range [a,b),
- * prints them, and then prints the minimal number that was generated.
- */
-public class GenThree {
-	public static void main(String[] args) {
-		// Put your code here	
+public class Gen3
+{
+	public static void main(String[] args)
+	{
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int min = b;
+		for (int i=1; i<=3; i++)
+		{
+			int num1 = (int)((Math.random()*(a-b+1))+b);
+			System.out.println(num1);
+			min = Math.min(num1,min);
+		}
+		 System.out.println("The minimal generates numbar was " + min);
+
 	}
 }
