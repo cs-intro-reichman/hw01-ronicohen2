ce6bf02dd81fb60e368868d33ef169104a1591e3
ronicohen2 <154340689+ronicohen2@users.noreply.github.com>
2023-12-21 14:55:59 +0200

    Adding commit bla bla

--- AddTwo.java
+++ AddTwo.java
@@ -1,8 +1,9 @@
-/*
- * Adds two given integers and prints the result in a fancy way.
- */
-public class AddTwo {
-	public static void main(String[] args) {
-		// Put your code here	
+public class AddTwo
+{
+	public static void main(String[] args)
+	{
+		int x = Integer.parseInt(args[0]);
+		int y = Integer.parseInt(args[1]);
+		System.out.println(x + " + " + y + " = " + (x+y));
 	}
 }
