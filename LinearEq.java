a066741dd449cb10b44432521c5c00cde221bcf4
ronicohen2 <154340689+ronicohen2@users.noreply.github.com>
2023-12-21 15:23:52 +0200

    update

--- LinearEq.java
+++ LinearEq.java
@@ -7,6 +7,6 @@ public class LinearEq
 		double c = Double.parseDouble(args[2]);
 		double x = (c-b)/a;
 		System.out.println(a + " * x + " + b + " = " + c);
-		System.out.println("x =" + x);
+		System.out.println("x = " + x);
 	}
 }
